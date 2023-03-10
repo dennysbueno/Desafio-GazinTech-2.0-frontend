7cd4585bc11c15fddc607b6b161472a519fdbf70
dennysbueno <dennys.bueno8@gmail.com>
2023-03-10 11:28:28 -0300

    :recycle: refactor: changed Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { Box, CSSReset, Grid, ThemeProvider } from "@chakra-ui/react";
-import React from "react";
+import { Box, ChakraProvider, CSSReset, Grid, ThemeProvider } from "@chakra-ui/react";
 import { Main } from "./pages/Main";
 import theme from "./theme";
 
 const App = () => (
-  <ThemeProvider theme={theme}>
+  <ChakraProvider theme={theme}>
     <CSSReset />
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh">
         <Main />
       </Grid>
     </Box>
-  </ThemeProvider>
+  </ChakraProvider>
 );
 
 export default App;
