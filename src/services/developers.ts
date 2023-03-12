9cab2c28770e53e6d4aea8158cb8a889efd58bd4
dennysbueno <dennys.bueno8@gmail.com>
2023-03-12 15:08:21 -0300

    :sparkles: feat: added function for calculate age

--- src/services/developers.ts
+++ src/services/developers.ts
@@ -1,5 +1,9 @@
 import { api } from "./index";
-import { DevelopersRequestProps } from "../interfaces/developers";
+import {
+  DevelopersRequestProps,
+  DevelopersResponseProps,
+} from "../interfaces/developers";
+import { differenceInYears, parseISO } from "date-fns";
 
 export async function findAllDevelopers() {
   return api.get("/developers");
@@ -22,4 +26,12 @@ export async function updateDeveloper(
   developer: DevelopersRequestProps
 ) {
   return api.put(`developer/${id}`, developer);
+}
+
+export function calculateAge(developer: DevelopersResponseProps): number {
+  const birthdate = parseISO(developer.birthdate);
+  const today = new Date();
+  const age = differenceInYears(today, birthdate);
+
+  return age;
 }
